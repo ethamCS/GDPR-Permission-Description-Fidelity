1e942c847c8b325a9c3ab8d24505825fb9341a0a
Ricky Hernandez <rh358@njit.edu>
2023-05-31 16:45:32 -0400

    Eliminated CSV empty spaces

--- text_file_intake2.py
+++ text_file_intake2.py
@@ -1,5 +1,4 @@
 from google_play_scraper import app
-from google_play_scraper import permissions
 import pandas as pd
 
 file_path = './app_ids2.txt'
@@ -15,34 +14,33 @@ df = pd.DataFrame(columns=['App_Name', 'App_ID', 'Sentence'])
 
 app_id_count = 1
 
-
 for app_name in app_List:
     if app_name == None: 
         continue
     try:
         result_app_details = app(
-        app_name,
-        lang='en',  # defaults to 'en'
-        country='eu'  # defaults to 'us'
+            app_name,
+            lang='en',  # defaults to 'en'
+            country='eu'  # defaults to 'us'
         )
     except: 
         print(app_name)
         print("error: app not found")
         continue
 
-
     description = result_app_details['description']
-    # tokenizeing by new lines
+    # tokenizing by new lines
     tokens = description.split('\n')
- 
 
     for sentence in tokens:
-         #see what is causing empty lines, then make a conditional for it
-        if len(sentence) > 0:  # Check if the sentence is not an empty string after stripping whitespace
+        if sentence.strip():  # Check if the sentence is not an empty string after stripping whitespace
             df.loc[len(df.index)] = [app_name, app_id_count, sentence.strip()]
 
     app_id_count += 1
     print(df)
 
-# or parse the csv and remove empty rows 
-df.to_csv('./test2.csv')
+# Remove empty rows from the DataFrame
+df = df[df['Sentence'].str.len() > 0]
+
+# Save the DataFrame to a CSV file
+df.to_csv('./test2.csv', index=False)
