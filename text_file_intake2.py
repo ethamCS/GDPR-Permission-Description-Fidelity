a0c880761f9f6e6df11d982ed1efc8956850b036
ethamCS <76605572+ethamCS@users.noreply.github.com>
2023-06-01 15:29:17 -0600

    rewrote some code in text_file_intake2.py

--- text_file_intake2.py
+++ text_file_intake2.py
@@ -1,45 +1,74 @@
 from google_play_scraper import app
 import pandas as pd
+import sys
 
-file_path = './app_ids2.txt'
+app_list = []
 
-def read_qualified_names(file_path):
-    with open(file_path, 'r') as file:
-        app_List = file.read().split("\n")
-    return app_List
+def read_file():
+    if len(sys.argv) < 2:
+        print("Usage: python3 text_file_intake.py filename")
+        sys.exit("Error: no file provided")
 
-app_List = read_qualified_names(file_path)
+    filename = sys.argv[1]
 
-df = pd.DataFrame(columns=['App_Name', 'App_ID', 'Sentence'])
-
-app_id_count = 1
-
-for app_name in app_List:
-    if app_name == None: 
-        continue
     try:
-        result_app_details = app(
-            app_name,
-            lang='en',  # defaults to 'en'
-            country='eu'  # defaults to 'us'
-        )
-    except: 
-        print(app_name)
-        print("error: app not found")
-        continue
+        with open(filename, 'r') as file:
+            app_list = file.read().split("\n")
+        return app_list
+    except FileNotFoundError:
+        msg = f'Error: "{filename}" could not be found'
+        sys.exit(msg)
 
-    description = result_app_details['description']
-    # tokenizing by new lines
-    tokens = description.split('\n')
+def main():
+    apps = read_file()
+    build_dataset(apps)
 
-    for sentence in tokens:
-        if sentence.strip():  # Check if the sentence is not an empty string after stripping whitespace
-            df.loc[len(df.index)] = [app_name, app_id_count, sentence.strip()]
+# def read_qualified_names(file_path):
+#     with open(file_path, 'r') as file:
+#         app_List = file.read().split("\n")
+#     return app_List
 
-    app_id_count += 1
 
-# Remove empty rows from the DataFrame
-df = df[df['Sentence'].str.len() > 0]
+# app_List = read_qualified_names(file_path)
 
-# Save the DataFrame to a CSV file
-df.to_csv('./test2.csv', index=False)
+def build_dataset(apps):
+    
+    df = pd.DataFrame(columns=['App_Name', 'App_ID', 'Sentence'])
+
+    app_id_count = 1
+
+    for app_name in apps:
+        if app_id_count > 10:
+            break
+        if app_name == None:
+            continue
+        try:
+            result_app_details = app(
+                app_name,
+                lang='en',  # defaults to 'en'
+                country='eu'  # defaults to 'us'
+            )
+        except:
+            print(app_name)
+            print("error: app not found")
+            continue
+
+        description = result_app_details['description']
+        # tokenizing by new lines
+        tokens = description.split('\n')
+
+        for sentence in tokens:
+            if sentence.strip():  # Check if the sentence is not an empty string after stripping whitespace
+                df.loc[len(df.index)] = [app_name, app_id_count, sentence.strip()]
+            print(df)
+        app_id_count += 1
+
+    # Remove empty rows from the DataFrame
+    df = df[df['Sentence'].str.len() > 0]
+
+    # Save the DataFrame to a CSV file
+    df.to_csv('./datatset.csv', index=False)
+
+
+if __name__ == '__main__':
+    main()
